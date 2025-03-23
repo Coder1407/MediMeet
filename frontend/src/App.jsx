9affb1c08857c6ad94a0c76294afd8b0b2d47a85
Coder1407 <amitesh1407@gmail.com>
2025-03-23 17:33:04 +0530

    frontend added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -21,10 +22,11 @@ function App() {
         <Route path='/login' element={ <Login /> }/>
         <Route path='/about' element={ <About /> }/>
         <Route path='/contact' element={ <Contact /> }/>
-        <Route path='/myprofile' element={ <MyProfile /> }/>
-        <Route path='/myappointments' element={ <MyAppointments /> }/>
+        <Route path='/my-profile' element={ <MyProfile /> }/>
+        <Route path='/my-appointments' element={ <MyAppointments /> }/>
         <Route path='/appointment/:docId' element={ <Appointment /> }/>
       </Routes>
+      <Footer />
     </div>
   )
 }
