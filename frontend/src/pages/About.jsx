3ffa8be4e627858a439d01e34b92191df50d818f
Coder1407 <amitesh1407@gmail.com>
2025-03-23 20:49:03 +0530

    minor change

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           <p>Welcome to MediMeet, your trusted partner in managing your healthcare needs conveniently and efficiently. At MediMeet, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
           <p>MediMeet is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, MediMeet is here to support you every step of the way.</p>
           <b className='text-gray-800'>Our Vision</b>
-          <p>Our vision at Prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+          <p>Our vision at MediMeet is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
         </div>
       </div>
 
